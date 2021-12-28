418711e29d6406992b59ca53c84772db555301b5
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2021-12-28 11:36:50 +0300

    add chart

--- src/classes/order_item.ts
+++ src/classes/order_item.ts
@@ -3,13 +3,13 @@ import {Entity} from "@/interfaces/entity";
 export class OrderItem implements Entity {
     id: number;
     product_title: string;
-    price: number;
+    product_price: number;
     quantity: number;
 
-    constructor(id = 0, product_title = '', price = 0, quantity = 0) {
+    constructor(id = 0, product_title = '', product_price = 0, quantity = 0) {
         this.id = id;
         this.product_title = product_title;
-        this.price = price;
+        this.product_price = product_price;
         this.quantity = quantity;
     }
 }
